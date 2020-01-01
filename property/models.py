11bbaec747dbb8ef3702f880392c7af106d6e9e5
Nivedita Bhat <nivedita@LT345.local>
2020-01-01 14:56:22 +0100

    add name for 'property' model

--- property/models.py
+++ property/models.py
@@ -48,11 +48,17 @@ class Property(models.Model):
         default='unfurnished'
     )
 
+    class Meta:
+        verbose_name_plural = 'Properties under HousingSolutions'
+
+    def __str__(self):
+        return self.name
+
 
 class PropertyPrice(models.Model):
     property = models.OneToOneField(
-        Property, related_name="property_price", help_text="Property", on_delete=models.CASCADE, default=100)
+        Property, related_name="property_price", help_text="Property", on_delete=models.CASCADE, default=None)
     rent = models.DecimalField(
-        help_text="Property rent (in €)", max_digits=6, decimal_places=2, default=100.00)
+        help_text="Property rent (in €)", max_digits=6, decimal_places=2, default=00.00)
     deposit = models.DecimalField(
-        help_text="Property deposit (in €)", max_digits=6, decimal_places=2, default=100.00)
+        help_text="Property deposit (in €)", max_digits=6, decimal_places=2, default=00.00)
