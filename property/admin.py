17f51324cbcf58f0b035a17ead8a283e2635c631
Nivedita Bhat <nivedita@LT345.local>
2020-01-01 11:49:53 +0100

    add address, price and image models as inline admin classes to show all the models related to 'property' model together

--- property/admin.py
+++ property/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from common.models import Address, Image
+from property.models import Property, PropertyPrice
+from django.utils.html import escape
+from django.utils.html import mark_safe
 
-# Register your models here.
+
+class AddressInline(admin.StackedInline):
+    model = Address
+
+
+class PropertyPriceInline(admin.StackedInline):
+    model = PropertyPrice
+
+
+class ImageInline(admin.StackedInline):
+    model = Image
+    readonly_fields = ["image_tag"]
+
+    def image_tag(self, obj):
+        return mark_safe('<img src="{url}" width="200" height="300" />'.format(
+            url=obj.image.url,
+            width=obj.image.width,
+            height=obj.image.height,
+        )
+        )
+
+    image_tag.short_description = 'Preview'
+    image_tag.allow_tags = True
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    inlines = [AddressInline, PropertyPriceInline, ImageInline]
