82b0e7925e03287a626b5442ecb8d65b2208b328
Nivedita Bhat <nivedita@LT345.local>
2020-01-01 19:38:56 +0100

    add relations, 'Property' and 'Customer' to booking model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from property.models import Property
+from customer.models import Customer
 
-# Create your models here.
+
+class Booking(models.Model):
+    property = models.ForeignKey(
+        Property, related_name="booking", help_text="Property", on_delete=models.CASCADE, null=True)
+    customer = models.OneToOneField(
+        Customer, related_name="booking", help_text="Customer", on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = 'Booking'
+
+    def __str__(self):
+        return "Booking {id} : {name}".format(name=self.property.name, id=self.id)
