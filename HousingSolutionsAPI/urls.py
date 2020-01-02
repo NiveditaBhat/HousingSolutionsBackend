ae3b5e2da99d6afd2c7a7c1b2560722acb65d4d3
Nivedita Bhat <nivedita@LT345.local>
2020-01-02 14:14:45 +0100

    change  titles for admin page

--- HousingSolutionsAPI/urls.py
+++ HousingSolutionsAPI/urls.py
@@ -19,3 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "HousingSolutions Admin"
+admin.site.site_title = "HousingSolutions Admin Portal"
+admin.site.index_title = "Welcome to HousingSolutions Admin Portal"
