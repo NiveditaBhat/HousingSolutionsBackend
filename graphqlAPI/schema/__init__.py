6b2a7654327d82c5add974da30ca511b8d41acbc
Nivedita Bhat <nivedita@LT345.local>
2020-01-03 10:43:44 +0100

    add root Query in the schema and add propertyQuery as its child

---
+++ graphqlAPI/schema/__init__.py
@@ -0,0 +1,19 @@
+from graphene import Field, ObjectType, Schema
+from .property import PropertyQuery
+
+
+def resolve_self(_, info):
+    """
+    Generic resolver used for namespacing the schemas.
+    """
+    return info.return_type.graphene_type()
+
+
+class Query(ObjectType):
+    """
+    Root query which includes all queries from other modules
+    """
+    property = Field(PropertyQuery, resolver=resolve_self)
+
+
+schema = Schema(query=Query)
