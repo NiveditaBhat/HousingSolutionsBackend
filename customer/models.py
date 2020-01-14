cc043309b658caef263734becac0d2a5714881ea
Nivedita Bhat <nivedita@LT345.local>
2020-01-14 14:21:39 +0100

    revert customer model , remove foreign key to booking

--- customer/models.py
+++ customer/models.py
@@ -2,15 +2,10 @@ from django.contrib.auth.models import User
 from django.db import models
 from phone_field import PhoneField
 
-from booking.models import Booking
-
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = PhoneField(blank=True, null=True, help_text='Phone number')
-    booking = models.OneToOneField(Booking, null=True,
-                                   on_delete=models.CASCADE,
-                                   help_text='Booking')
     
     class Meta:
         verbose_name_plural = 'Customers registered under HousingSolutions'
