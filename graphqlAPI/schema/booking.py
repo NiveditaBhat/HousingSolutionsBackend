b6e5d19239426ce4e05b9b1aeac4bb9732f9fd59
Nivedita Bhat <nivedita@LT345.local>
2020-01-17 16:19:37 +0100

    add error handling for booking mutations  createBooking and cancelBooking

--- graphqlAPI/schema/booking.py
+++ graphqlAPI/schema/booking.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
 
 from booking.models import Booking
 from customer.models import Customer
@@ -24,13 +25,14 @@ class CreateBooking(graphene.Mutation):
         booking_data = BookingInput(required=True)
     
     def mutate(self, info, booking_data):
-        if info.context.user.is_authenticated:
-            customer = Customer.objects.get(id=booking_data.customer_id)
-            property = Property.objects.get(id=booking_data.property_id)
-            booking = Booking(property=property, customer=customer,
-                              message=booking_data.booking_message)
-            booking.save()
-            return CreateBooking(booking=booking)
+        if info.context.user.is_anonymous:
+            raise GraphQLError('User not authenticated')
+        customer = Customer.objects.get(id=booking_data.customer_id)
+        property = Property.objects.get(id=booking_data.property_id)
+        booking = Booking(property=property, customer=customer,
+                          message=booking_data.booking_message)
+        booking.save()
+        return CreateBooking(booking=booking)
 
     
 class CancelBooking(graphene.Mutation):
@@ -41,12 +43,13 @@ class CancelBooking(graphene.Mutation):
         customer_id = graphene.String(required=True)
     
     def mutate(self, info, booking_id, customer_id):
-        if info.context.user.is_authenticated:
-            customer = Customer.objects.get(id=customer_id)
-            booking = Booking.objects.get(id=booking_id,
-                                          customer=customer)
-            booking.delete()
-            return CancelBooking(booking=booking)
+        if info.context.user.is_anonymous:
+            raise GraphQLError('User not authenticated')
+        customer = Customer.objects.get(id=customer_id)
+        booking = Booking.objects.get(id=booking_id,
+                                      customer=customer)
+        booking.delete()
+        return CancelBooking(booking=booking)
 
 
 class BookingMutation(graphene.ObjectType):
