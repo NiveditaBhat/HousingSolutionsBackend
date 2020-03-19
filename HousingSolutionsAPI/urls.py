ec078b373cb3743ded195eca2c66aee522702aed
Nivedita Bhat <nivedita@LT345.local>
2020-03-19 11:07:00 +0100

    add csfr excemption for enabling requests from frontend

--- HousingSolutionsAPI/urls.py
+++ HousingSolutionsAPI/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
+
 from graphene_django.views import GraphQLView
 
 from graphqlAPI.schema import schema
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
+    url(r'^graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
 
 admin.site.site_header = "HousingSolutions Admin"
